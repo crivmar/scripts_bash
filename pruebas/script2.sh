006d0f507455fcc3ebe1f1b4afd58ca440b60f2c
Carlos R <carlosrivero1988@gmail.com>
2021-06-07 18:03:16 +0000

    Este script no funciona correctamente en mi máquina, investigaré

---
+++ pruebas/script2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cadena= "Tempus edax rerum"  #Esto es una variable de texto.
+num= 10 	#Esto es una varible numérica
+comando= $(ps -ef | grep bash) # Esto es una variable que obtiene un valor de un comando.
+
+# Necesitas usar el símbolo $ para llamar a la variable
+
+echo $cadena
+echo $num
+echo $comando
+
+# Otras formas de operar numéricos 
+
+((num++)) 	#INCREMENTO
+((num+=2))	#SUMAR DOS
+
+echo $num
