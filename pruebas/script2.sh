7e9b7144ac83fdcea668bc3953c5bdf3d037679b
Carlos R <carlosrivero1988@gmail.com>
2021-06-08 07:36:33 +0000

    Correción de errores de sintaxis

--- pruebas/script2.sh
+++ pruebas/script2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cadena= "Tempus edax rerum"  #Esto es una variable de texto.
-num= 10 	#Esto es una varible numérica
-comando= $(ps -ef | grep bash) # Esto es una variable que obtiene un valor de un comando.
+cadena="Tempus edax rerum"  #Esto es una variable de texto.
+num=10 	#Esto es una varible numérica
+comando=`ps -ef | grep bash` # Esto es una variable que obtiene un valor de un comando.
 
 # Necesitas usar el símbolo $ para llamar a la variable
 
@@ -15,4 +15,4 @@ echo $comando
 ((num++)) 	#INCREMENTO
 ((num+=2))	#SUMAR DOS
 
-echo $num
+echo "Ahora num vale $num"
