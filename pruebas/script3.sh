5898b600122fef883106c52ad153a04328ba145e
Carlos R <carlosrivero1988@gmail.com>
2021-06-08 07:05:23 +0000

    Cambios

--- pruebas/script3.sh
+++ pruebas/script3.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
 
-lista=[Zeus, Ares, Afrodita, Hera, Poseidón, Hades, Atenea]
+lista=[Zeus, Afrodita, Ares, Hera, Poseidón, Hades, Atenea]
 
 echo $(lista[*]) 	# Devuelve todos los valores de la lista
+
+echo $(#lista[*])	# Devuelve el recuento de objetos de la lista
+
+echo $(#lista[1])	# Devielve la longitud del objeto uno, como se numeran desde 0, sería Afrodita
+
+
+# Si el úlitmo comando es igual a 0 ha finalizado bien, si da un fallo (es decir, devulve un 1) hace lo otro
+
+if [ $? -eq 0 ] 
+then
+	echo "El comando anterior ha finalizado OK"
+else
+	echo "El comando anterior ha fallado :("
+fi
