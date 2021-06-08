f7e6527a42720c69edae6dc76e87ac095680232d
Carlos R <carlosrivero1988@gmail.com>
2021-06-08 07:29:12 +0000

    Corrección de sintaxis

--- pruebas/script3.sh
+++ pruebas/script3.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-lista=[Zeus, Afrodita, Ares, Hera, Poseidón, Hades, Atenea]
+lista=(Zeus Afrodita Ares Hera Poseidón Hades Atenea)
 
-echo $(lista[*]) 	# Devuelve todos los valores de la lista
+echo ${lista[*]} 	# Devuelve todos los valores de la lista
 
-echo $(#lista[*])	# Devuelve el recuento de objetos de la lista
+echo ${#lista[*]}	# Devuelve el recuento de objetos de la lista
 
-echo $(#lista[1])	# Devielve la longitud del objeto uno, como se numeran desde 0, sería Afrodita
+echo ${#lista[1]}	# Devielve la longitud del objeto uno, como se numeran desde 0, sería Afrodita
 
 
 # Si el úlitmo comando es igual a 0 ha finalizado bien, si da un fallo (es decir, devulve un 1) hace lo otro
