1395fa9b03654a33c4356c9e190fd360ad2f7af0
Carlos R <carlosrivero1988@gmail.com>
2021-06-08 15:50:53 +0000

    Añadiendo comentarios

--- pruebas/script5.sh
+++ pruebas/script5.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-nom=$USER
+nom=$USER		# Variable de entorno nombre del usuario actual.
 
 dia=`date +%D`		#Indica el día en el que estamos, formato inglés.
 
-dir=$PWD
+dir=$PWD		# Variable entorno directorio de trabajo actual.
 
-
+# Si el directorio de trabajo actual no es /home dirá una cosa y si estás en él otra.
 if [ $dir != "/home" ]
-then	
+then
 	echo "Hola $nom , no estás en '/home' y hoy es $dia"
 else
 	echo "Hola $nom, estás en $dir y hoy es $dia"
